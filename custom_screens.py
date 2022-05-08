4b3f6ea3fc17f399df1133fd76679132c815592a
Jeferson Hernandez <j.hernandez@nelumboconsultores.com>
2022-05-07 22:24:45 -0500

    refactor: added base url to a sections items

--- custom_screens.py
+++ custom_screens.py
@@ -2,32 +2,74 @@ import enum
 
 from enums import Seccion, AuthLevel, Screen
 
+API = "http://127.0.0.1:8000/"
 
 available = [
-    {"title": "Inicio", "tabBarItem": "Inicio",
-        "seccion": Seccion.DESTACADOS, "screenType": Screen.VIEW, "authLevel": AuthLevel.PUBLIC},
-    {"title": "Realities", "tabBarItem": "Inicio",
-        "seccion": Seccion.REALITIES, "screenType": Screen.VIEW, "authLevel": AuthLevel.PUBLIC},
-    {"title": "Novelas", "tabBarItem": "Inicio",
-        "seccion": Seccion.NOVELAS, "screenType": Screen.VIEW, "authLevel": AuthLevel.PUBLIC},
-    {"title": "Programas", "tabBarItem": "Inicio",
-        "seccion": Seccion.PROGRAMAS, "screenType": Screen.VIEW, "authLevel": AuthLevel.PUBLIC},
-    {"title": "Cine", "tabBarItem": "Inicio",
-        "seccion": Seccion.CINE, "screenType": Screen.VIEW, "authLevel": AuthLevel.PUBLIC},
-    {"title": "A mas +", "tabBarItem": "Inicio",
-        "seccion": Seccion.AMAS, "screenType": Screen.VIEW, "authLevel": AuthLevel.PUBLIC},
-    {"title": "Series", "tabBarItem": "Inicio",
-        "seccion": Seccion.SERIES, "screenType": Screen.VIEW, "authLevel": AuthLevel.PUBLIC},
-    {"title": "KidSiete", "tabBarItem": "Inicio",
-        "seccion": Seccion.KIDSIETE, "screenType": Screen.WEBVIEW, "authLevel": AuthLevel.PUBLIC},
-    {"title": "Es mucho mas", "tabBarItem": "Inicio",
-        "seccion": Seccion.ESMUCHOMAS, "screenType": Screen.WEBVIEW, "authLevel": AuthLevel.PUBLIC}
+    {
+        "title": "Inicio",
+        "tabBarItem": "Inicio",
+        "seccion": f"{API}seccion/destacados",
+        "screenType": Screen.VIEW,
+        "authLevel": AuthLevel.PUBLIC
+    },
+    {
+        "title": "Realities",
+        "tabBarItem": "Inicio",
+        "seccion": f"{API}seccion/realities",
+        "screenType": Screen.VIEW,
+        "authLevel": AuthLevel.PUBLIC
+    },
+    {
+        "title": "Novelas",
+        "tabBarItem": "Inicio",
+        "seccion": f"{API}seccion/novelas",
+        "screenType": Screen.VIEW,
+        "authLevel": AuthLevel.PUBLIC
+    },
+    {
+        "title": "Programas",
+        "tabBarItem": "Inicio",
+        "seccion": f"{API}seccion/programas",
+        "screenType": Screen.VIEW,
+        "authLevel": AuthLevel.PUBLIC
+    },
+    {
+        "title": "Cine",
+        "tabBarItem": "Inicio",
+        "seccion": f"{API}seccion/cine",
+        "screenType": Screen.VIEW,
+        "authLevel": AuthLevel.PUBLIC
+    },
+    {
+        "title": "A mas +",
+        "tabBarItem": "Inicio",
+        "seccion": f"{API}seccion/amas",
+        "screenType": Screen.VIEW,
+        "authLevel": AuthLevel.PUBLIC
+    },
+    {
+        "title": "Series",
+        "tabBarItem": "Inicio",
+        "seccion": f"{API}seccion/series",
+        "screenType": Screen.VIEW,
+        "authLevel": AuthLevel.PUBLIC
+    },
+    {
+        "title": "KidSiete",
+        "tabBarItem": "Inicio",
+        "seccion": f"{API}seccion/kidsiete",
+        "screenType": Screen.WEBVIEW,
+        "authLevel": AuthLevel.PUBLIC
+    },
+    {
+        "title": "Es mucho mas",
+        "tabBarItem": "Inicio",
+        "seccion":  f"{API}seccion/realities",
+        "screenType": Screen.WEBVIEW,
+        "authLevel": AuthLevel.PUBLIC
+    }
 ]
 
 
 def getCustomScreens():
-    #available = []
-
-    # for i in range(10):
-    #    available.append({"title": f"Title {i}", "tabBarItem": "Inicio", "provider": "destacados", "screentype"})
     return available
